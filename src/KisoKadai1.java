92880e4ecb0e6c49b612cd245b3fd567faaf27d7
Yamazaki <t.yamazaki.ins@icloud.com>
2016-01-25 12:58:41 +0900

    kisokadai

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -1,20 +1,26 @@
-public class KisoKadai1
-{
-	  public static void main(String args[])
-	  {
-		    int hyou[][] = new int[9][9];
+public class KisoKadai1 {
 
-		    for( int i=0; i<9; i++ ) {
-		      for( int j=0; j<9; j++ ){
-		    	  hyou[i][j] = (i+1) * (j+1);
-		      }
-		    }
+		public static void main(String[] args) {
+		   System.out.println("九九表");
 
-		    for( int i=0; i<9; i++ ) {
-		      for( int j=0; j<9; j++ ) {
-		    	  System.out.print(hyou[i][j] + " ");
-		      }
-		      	  System.out.println();
-		    }
-	  }
+		   System.out.println("xの値を入れてください。");
+		   int x = new java.util.Scanner(System.in).nextInt();
+
+		   System.out.println("yの値を入れてください。");
+		   int y = new java.util.Scanner(System.in).nextInt();
+		   for( int i=1; i<=x; i ++) {
+	        for( int j=1; j<=y; j ++) {
+	    	  int value = i * j;
+	    	  System.out.print(i + "X" + j + "=");
+
+	    	  if(value < 10) {
+
+	    	System.out.print(" ");
+	    	  }
+
+	    	  System.out.print(value +" ");
+	       }
+	       System.out.println();
+	       	}
+	}
 }
---
+++ src/KisoKadai3.java
@@ -0,0 +1,146 @@
+	import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class KisoKadai3 {
+
+
+
+	  public static void main(String args[]){
+
+			    InputStreamReader a = new InputStreamReader(System.in);
+
+			    BufferedReader br = new BufferedReader(a);
+
+                int z;
+                int q=0;
+                int h=0;
+			    String k;
+				String y;
+				String j;
+				File nf;
+while(q==0){
+                z=0;
+				while(z==0){
+		            try{
+		            	System.out.println("");
+		                System.out.println("新規作成:1    上書き:2    追記:3    読み込み:4    終了:5");
+
+		                    String b = br.readLine();
+		                    h = Integer.parseInt(b);
+		                    if(h<6){z=1;}else{System.out.println("正しく数値を入力してください。");
+		                    }
+		                    }catch(Exception e){
+			                    System.out.println("エラー");z=0;
+		    	            }
+
+   int w = h;
+
+
+switch(w){
+
+case 1:
+
+
+    try{
+ 	   		System.out.println("新規ファイルを作ります。フォルダの場所を指定してください。");
+ 	   			k=br.readLine();
+
+
+
+
+
+
+ 	   		System.out.println("ファイル名を入力");
+ 	   			y = br.readLine();
+ 	   			nf = new File(y);
+
+ 	   			if (nf.createNewFile()){
+ 	   				System.out.println("ファイルの作成に成功しました");
+ 	   					}else{
+ 	   				System.out.println("ファイルの作成に失敗しました");
+ 	   					}
+
+ 	   			FileWriter fr = new FileWriter(k+"/"+y);
+
+ 	   			System.out.println("内容を入力");
+ 	   			   j=br.readLine();
+ 	   			   fr.write(j);
+ 	   			System.out.println("成功");
+ 	   			   fr.close();
+    }catch(IOException e){
+ 	   	System.out.println("エラー");
+    }
+    break;
+
+
+case 2:   try{
+	            System.out.println("既存のファイルに上書きします。フォルダのパスを入力");
+                       k=br.readLine();
+
+                FileWriter fr = new FileWriter(k);
+
+
+                System.out.println("内容を入力");
+                       j=br.readLine();
+                       fr.write(j);
+                System.out.println("成功");
+                       fr.close();
+          }catch(IOException e){
+                System.out.println("エラー");
+              }
+        break;
+
+case 3:    try{
+
+  	            System.out.println("追記するファイルのパスを入力");
+                    k=br.readLine();
+  	            FileWriter fr = new FileWriter(k,true);
+
+
+                System.out.println("内容を入力");
+                    j=br.readLine();
+	                fr.write(j);
+                System.out.println("成功");
+	                fr.close();
+
+            }catch(IOException e){
+                System.out.println("エラー");}
+
+             break;
+
+
+case 4:    try{
+                     y=br.readLine();
+
+                File f = new File(y);
+                FileReader fd = new FileReader(f);
+
+                  int ch = fd.read();
+                 while((ch = fd.read())!=-1){
+                                      System.out.print((char)ch);
+                                  }
+                 fd.close();
+          }catch(FileNotFoundException n){
+                               System.out.println(n);
+          }catch(IOException n){
+                               System.out.println(n);
+          }
+
+        break;
+case 5:  System.out.println("Bye");q=1;
+        break;
+
+		  }
+
+	          }
+
+
+
+	  }
+	  }
+}
